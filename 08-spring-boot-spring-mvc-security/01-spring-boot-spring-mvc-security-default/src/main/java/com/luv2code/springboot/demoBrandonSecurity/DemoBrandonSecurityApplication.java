abe89c6e2b7fb2ad2f44e702056c7dff59b4ac2c
brandonibanez <ibanezbrandonn@gmail.com>
2024-01-27 23:03:34 +0800

    Spring MVC Security
    -SQL users & roles configuration
    -Custom login form
    -Login form error message for invalid username/password

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demoBrandonSecurity/DemoBrandonSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demoBrandonSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBrandonSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBrandonSecurityApplication.class, args);
+	}
+
+}
